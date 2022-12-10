026d9765f38b63e7944b44552931d04876199591
Jasonccase <60761432+Jasonccase@users.noreply.github.com>
2022-12-09 23:49:02 -0500

    Update turtle.lua

--- turtle.lua
+++ turtle.lua
@@ -1,29 +1,33 @@
-
-while(not(turtle.detect)) do
-  turtle.forward()
+while(true) then
+  while(turtle.getFuelLevel()<10000) then
+    turtle.refuel()
+    end
+  while(not(turtle.detect)) do
+    turtle.forward()
+    end
+  while(turtle.detect) then
+    for i=0,5,1 do
+      turtle.dig()
+      turtle.forward()
+      end
+    turtle.turnRight()
+    for i=0,11,1 do
+      turtle.dig()
+      turtle.forward()
+      end
+    turtle.turnRight()
+    turtle.turnRight()
+    for i=0,11,1 do
+      turtle.forward()
+      end
+    for i=0,11,1 do
+      turtle.dig()
+      turtle.forward()
+      end
+    turtle.turnRight()
+    turtle.turnRight()
+    for i=0,11,1 do
+      turtle.forward()
+      end
+    turtle.turnLeft()
   end
-if(turtle.detect) then
-  for i=0,5,1 do
-    turtle.dig()
-    turtle.forward()
-    end
-  turtle.turnRight()
-  for i=0,11,1 do
-    turtle.dig()
-    turtle.forward()
-    end
-  turtle.turnRight()
-  turtle.turnRight()
-  for i=0,11,1 do
-    turtle.forward()
-    end
-  for i=0,11,1 do
-    turtle.dig()
-    turtle.forward()
-    end
-  turtle.turnRight()
-  turtle.turnRight()
-   for i=0,11,1 do
-    turtle.forward()
-    end
-  turtle.turnLeft()
