7434355b54dc052cb30812d1734d953105758a0e
AJSmyth <AJSmyth@users.noreply.github.com>
2022-12-09 21:30:36 -0800

    Update turtle.lua

--- turtle.lua
+++ turtle.lua
@@ -1,10 +1,4 @@
 while(true) do
-  while(turtle.getFuelLevel()<10000) do
-    turtle.refuel()
-    end
-  while(not(turtle.detect)) do
-    turtle.forward()
-    end
   while(turtle.detect) do
     for i=0,5,1 do
       turtle.dig()
