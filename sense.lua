72424709dfef5e7c045913704459ee1bc598d511
AJSmyth <AJSmyth@users.noreply.github.com>
2022-12-09 20:39:01 -0800

    Update sense.lua

--- sense.lua
+++ sense.lua
@@ -1,15 +1,24 @@
-mine_block = {
-  [56] = true,        --diamond ore
-  [4474] = true,      --copper ore
-  [5192] = true      --tin ore
-}
+function sense()
+  mine_block = {
+    [56] = true,        --diamond ore
+    [4474] = true,      --copper ore
+    [5192] = true      --tin ore
+  }
 
---look left
-turtle.turnLeft()
-checkAndMine(turtle.sense())
-
-
-
+  --look left and scan
+  turtle.turnLeft()
+  checkAndMine(turtle.inspect())
+  
+  --look right and scan
+  turtle.turnRight()
+  turtle.turnRight()
+  checkAndMine(turtle.inspect())
+  
+  --return forward and scan up and down
+  turtle.turnLeft()
+  checkAndMine(turtle.inspectUp())
+  checkAndMine(turtle.inspectDown())
+end
 
 function checkAndMine(id)
   if (mine_block[id] == true) then
