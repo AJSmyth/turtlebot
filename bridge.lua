832301ef646525db86c7bb561ea3e87bf5cbb6f2
AJSmyth <AJSmyth@users.noreply.github.com>
2022-12-15 10:10:39 -0800

    Update bridge.lua

--- bridge.lua
+++ bridge.lua
@@ -1,7 +1,7 @@
 while (true) do
   if turtle.getItemCount(turtle.getSelectedSlot()) == 0 then
-    if (turtle.getSelectedSlot() == 16) then turtle.select(1) end
-    else then turtle.select(turtle.getSelectedSlot() + 1) end
+    if (turtle.getSelectedSlot() == 16) then turtle.select(1)
+    else turtle.select(turtle.getSelectedSlot() + 1) end
   else
     turtle.forward()
     turtle.placeDown()
