c494e9f20d7ca3e031ecf43556d2d73383757c46
AJSmyth <AJSmyth@users.noreply.github.com>
2022-12-16 17:59:41 -0800

    Update train.lua

--- train.lua
+++ train.lua
@@ -3,27 +3,37 @@ distances = {}
 colors = {}
 stopNum = 1
 
+for i = 0,10 do a[i] = 0 end
+
 term.clear()
 term.setCursorBlink(false)
-term.setBackgroundColor(colors.black)
+term.setBackgroundColor(32768)
 
 function addStop(name, distance, color)
   stops[stopNum] = name
   distances[stopNum] = distance
   colors[stopNum] = color
-  stopNum++
+  stopNum = stopNum + 1
 end
 
 function drawStops()
   x, y = term.getSize()
-  for L = 0,x,math.floor(x/stopNum) do 
-      R = L + math.floor(x/stopNum)
-      paintUtils.drawFilledBox(L,0,R,y,colors[stopNum])
+  for i = 1,stopNum do 
+      R = i * math.floor(x/stopNum)
+      L = R - math.floor(x/stopNum)
+      paintUtils.drawFilledBox(L,0,R,y,colors[i])
       term.setCursorPos(L+1,0)
-      term.setTextColor(colors[stopNum])
-      term.write(stops[stopNum])
+      term.setTextColor(colors[i])
+      term.write(stops[i])
       term.setCursorPos(L+1,1)
-      term.write(tostring(distances) .. " m")
+      term.write(tostring(distances[i]) .. " m")
    end
 end
       
+addStop("Blaze Farm",300,colors.orange)
+addStop("Cherry",1000,colors.red)
+drawStops()
+
+while true do
+  os.sleep(10)
+end
